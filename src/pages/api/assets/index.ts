c6e79a0fca93fb59b8e5e4d3e197144c52d5ebe2
Tim Kieu <timkieu8288@gmail.com>
2023-12-12 21:55:02 +0700

    up

--- src/pages/api/assets/index.ts
+++ src/pages/api/assets/index.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { fileUpload } from "@/utils/server";
-import { Request, Response } from "express";
+// import { Request, Response } from "express";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export const config = {
